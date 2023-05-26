5278de2ec7340aa964ea7a7745f0555183ca2033
MICHELE LOTTO <875922@stud.unive.it>
2023-05-26 03:55:49 +0200

    test done plots

--- all-doc-pairs-sim.py
+++ all-doc-pairs-sim.py
@@ -2,11 +2,16 @@ from utility import data_preparation
 
 import numpy as np
 
+import findspark
+findspark.init()
+
+from pyspark.conf import SparkConf
 from pyspark.sql import SparkSession
 
 import itertools
 
 import time
+import multiprocessing
 
 import pandas as pd
 
@@ -34,11 +39,19 @@ def sequential(doc_matrix, keys, threshold):
     
     return to_return, (stop_seq-start_seq)
     
-def spark_(doc_matrix:np.array, keys, threshold, n_workers=8, n_slices=5):
+def spark_(doc_matrix:np.array, keys, threshold_broadcast, n_workers=8, n_slices=5):
     
     # Create SparkSession 
-    spark = SparkSession.builder.master("local["+str(n_workers)+"]").appName("all-doc-pairs-similarity.com").config("spark.driver.memory", "10g").getOrCreate()
-    sc = spark.sparkContext
+    spark = SparkSession\
+    .builder\
+    .config(conf = SparkConf().setMaster(f"local[{n_workers}]") \
+        .setAppName("all_pairs_docs_similarity.com") \
+        .set("spark.executor.memory", "10g") \
+        .set("spark.executor.cores", "1") \
+        .set("spark.driver.memory", "10g"))\
+    .getOrCreate()
+
+    sc = spark.sparkContext # Get sparkContextxt
     
     term_freq = np.sum(doc_matrix>0, axis=0)
     sorted_terms_indexes = np.argsort(term_freq)[::-1]
@@ -46,7 +59,7 @@ def spark_(doc_matrix:np.array, keys, threshold, n_workers=8, n_slices=5):
     
     start_spark=time.perf_counter()
     
-    threshold=sc.broadcast(threshold)
+    threshold_broadcast=sc.broadcast(threshold_broadcast)
     
     keys_rdd=sc.parallelize(keys, n_workers*n_slices)
     vectorized_docs_rdd=keys_rdd.zip(sc.parallelize(doc_matrix, n_workers*n_slices)).persist()
@@ -55,7 +68,7 @@ def spark_(doc_matrix:np.array, keys, threshold, n_workers=8, n_slices=5):
         
         return np.maximum(doc1,doc2)
     
-    d_star=sc.broadcast(vectorized_docs_rdd.values().reduce(compute_d_star))
+    d_star_broadcast=sc.broadcast(vectorized_docs_rdd.values().reduce(compute_d_star))
     
     def Map(doc_pair):
     
@@ -65,12 +78,12 @@ def spark_(doc_matrix:np.array, keys, threshold, n_workers=8, n_slices=5):
         
         term=0
         
-        while(dot_prod<threshold.value):
+        while(dot_prod<threshold_broadcast.value):
             
             if term >= doc.shape[0]: 
                 return []
             
-            dot_prod+=((doc[term])*(d_star.value[term]))
+            dot_prod+=((doc[term])*(d_star_broadcast.value[term]))
             
             term+=1
         
@@ -90,7 +103,7 @@ def spark_(doc_matrix:np.array, keys, threshold, n_workers=8, n_slices=5):
         
         return np.nonzero(doc)[0]
     
-    doc_id_set_term=sc.broadcast(dict(vectorized_docs_rdd.mapValues(term_set).collect()))
+    doc_id_set_term_broadcast=sc.broadcast(dict(vectorized_docs_rdd.mapValues(term_set).collect()))
     
     def filter_pairs(pair):
         
@@ -100,8 +113,8 @@ def spark_(doc_matrix:np.array, keys, threshold, n_workers=8, n_slices=5):
         
         for id1, id2 in itertools.combinations(id_list,2):
             
-            common_terms=np.intersect1d(doc_id_set_term.value[id1],
-                                        doc_id_set_term.value[id2],
+            common_terms=np.intersect1d(doc_id_set_term_broadcast.value[id1],
+                                        doc_id_set_term_broadcast.value[id2],
                                         assume_unique=True)
             
             if len(common_terms)!=0 and max(common_terms)==term_id:
@@ -125,15 +138,13 @@ def spark_(doc_matrix:np.array, keys, threshold, n_workers=8, n_slices=5):
         
         _, similarity = pair
         
-        return similarity >= threshold.value
+        return similarity >= threshold_broadcast.value
 
     similarity_doc_pairs=doc_ids_pairs_rdd.map(Reduce).filter(similar_doc)
     
     to_return = similarity_doc_pairs.collect()
     
     end_spark=time.perf_counter()
-    
-    spark.stop()
     
     return to_return, (end_spark-start_spark)
     
@@ -155,7 +166,7 @@ def comparison(keys, doc_matrix, threshold:float, n_workers, n_slices):
 
 def test():
     
-    dataset = "scifact"
+    dataset = "nfcorpus"
     
     data = {
             "threshold":[],
@@ -167,18 +178,19 @@ def test():
         
     keys, doc_matrix = data_preparation(dataset,1000)
     
-    for threshold in [0.25, 0.5, 0.75]:
+    for threshold in [0.2, 0.4, 0.6]:
         
         for n_workers in [1, 2, 4, 6, 8, 12, 16]:
             
-            for n_slices in [1, 3, 4, 5, 7, 9]:
+            for n_slices in [1, 3, 9]:
                 
                 print("\n\nTEST FOR:")
                 print("threshold: ",threshold)
                 print("n_workers: ",n_workers)
                 print("n_slices: ",n_slices,"\n")
                 
-                spark_time, seq_time = comparison(keys, doc_matrix, threshold, n_workers, n_slices)
+                with multiprocessing.Pool(1) as p:
+                    spark_time, seq_time=p.apply(comparison, (keys, doc_matrix, threshold, n_workers, n_slices,))
                 
                 data["threshold"].append(threshold)
                 data["n_workers"].append(n_workers)
