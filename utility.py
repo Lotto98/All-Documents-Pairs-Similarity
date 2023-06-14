b192704707405e8acca5f4a7fef0c54fa56fb88b
Michele Lotto <875922@stud.unive.it>
2023-06-14 12:48:33 +0200

    prova

--- utility.py
+++ utility.py
@@ -1,22 +1,23 @@
-#data download
+# data download
 from beir.datasets.data_loader import GenericDataLoader
 from beir import util
 import os
 import pathlib
 
-#typing
+# typing
 from typing import List, Tuple
 from scipy.sparse import csr_matrix
 
-#text cleaning
+# text cleaning
 import spacy
-from multiprocessing import Pool
+from multiprocessing import Pool, cpu_count
 from tqdm.autonotebook import tqdm
 
-#documents vectorization
+# documents vectorization
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-def _data_download(dataset: str) -> Tuple[List[str],List]:
+
+def _data_download(dataset: str) -> Tuple[List[str], List]:
     """
     Given the dataset's name, download it from beir.
     The document title is prepended to the document text 
@@ -29,23 +30,25 @@ def _data_download(dataset: str) -> Tuple[List[str],List]:
             -corpus (List[str]): list of document texts. Each document is a string.
             -keys (List): list of document ids.
     """
-    
-    #Download dataset and unzip the dataset
+
+    # Download dataset and unzip the dataset
     url = "https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{}.zip".format(dataset)
     out_dir = os.path.join(pathlib.Path(os.path.abspath('')), "datasets")
     data_path = util.download_and_unzip(url, out_dir)
-    
-    #Retrieve documents
-    documents, _, _ = GenericDataLoader(data_path).load(split="test")
-    
-    return [document["title"]+" "+document["text"] for document in documents.values()], list(documents.keys())
 
-#load the spacy model for lemmatization 
-_nlp = spacy.load("en_core_web_lg",disable=['parser','ner'])
-    
+    # Retrieve documents
+    documents, _, _ = GenericDataLoader(data_path).load(split="test")
+
+    return [document["title"] + " " + document["text"] for document in documents.values()], list(documents.keys())
+
+
+# load the spacy model for lemmatization
+_nlp = spacy.load("en_core_web_lg", disable=['parser', 'ner'])
+
+
 def _cleaner(text: str) -> str:
     """
-    Auxillary function for text cleaning. Given a str, it apply cleaning and lemmatization using spacy library.
+    Auxiliary function for text cleaning. Given a str, it applies cleaning and lemmatization using spacy library.
     
     Args:
         text (str): text to clean.
@@ -54,6 +57,7 @@ def _cleaner(text: str) -> str:
         str: cleaned text.
     """
     return " ".join([token.lemma_.lower() for token in _nlp(text) if not token.is_stop and not token.is_punct])
+
 
 def _corpus_cleaning(corpus: List[str]) -> List[str]:
     """
@@ -65,11 +69,13 @@ def _corpus_cleaning(corpus: List[str]) -> List[str]:
     Returns:
         List[str]: cleaned corpus.
     """
+    # print(cpu_count())
     with Pool() as p:
-        cleaned_corpus=list(tqdm( p.imap(_cleaner, corpus), 
-                                                total=len(corpus),
-                                                desc="documents cleaning"))
+        cleaned_corpus = list(tqdm(p.imap(_cleaner, corpus),
+                                   total=len(corpus),
+                                   desc="documents cleaning"))
     return cleaned_corpus
+
 
 def _getVectorized(cleaned_corpus: List[str]) -> csr_matrix:
     """
@@ -83,10 +89,11 @@ def _getVectorized(cleaned_corpus: List[str]) -> csr_matrix:
         csr_matrix: vectorized corpus, in Compressed Sparse Row matrix format.
         Each row represent a document and each colum a specif term in the corpus.
     """
-    
+
     return TfidfVectorizer().fit_transform(cleaned_corpus)
 
-def data_preparation(dataset: str, limit:int=None)->Tuple[List,csr_matrix]:
+
+def data_preparation(dataset: str, limit: int = None) -> Tuple[List, csr_matrix]:
     """
     Data preparation function. Given a dataset name, it:
     -downloads the dataset.
@@ -103,15 +110,15 @@ def data_preparation(dataset: str, limit:int=None)->Tuple[List,csr_matrix]:
             -keys (List): list of document ids.
             -docs_matrix (csr_matrix): vectorized corpus.
     """
-    
+
     corpus, keys = _data_download(dataset)
-    
+
     if limit is not None:
         corpus = corpus[0:limit]
         keys = keys[0:limit]
-    
+
     cleaned_corpus = _corpus_cleaning(corpus)
-    
+
     doc_matrix = _getVectorized(cleaned_corpus)
-    
+
     return keys, doc_matrix
